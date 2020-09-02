4fdc8edaf94e5605f2a96209d77630d285f17140
Edward Greenberg <ed.w.greenberg@gmail.com>
2020-09-01 22:57:14 -0400

    Updates

---
+++ precache-manifest.8b60af5ae8f179402a159050be5b7e86.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf502105dba35f034f5eee3c22312b1",
+    "url": "/cardflip/index.html"
+  },
+  {
+    "revision": "49783a054ee49aa2c114",
+    "url": "/cardflip/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "b16b69af64c7327f880f",
+    "url": "/cardflip/static/js/2.6d0676a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cardflip/static/js/2.6d0676a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49783a054ee49aa2c114",
+    "url": "/cardflip/static/js/main.54f478c2.chunk.js"
+  },
+  {
+    "revision": "95adcb02c79813168f79",
+    "url": "/cardflip/static/js/runtime-main.a78a245b.js"
+  },
+  {
+    "revision": "36551848e507c3a167b85727c5d03a9f",
+    "url": "/cardflip/static/media/10C.36551848.svg"
+  },
+  {
+    "revision": "8a2acbb2a1d7445a7374f20c028c9640",
+    "url": "/cardflip/static/media/10D.8a2acbb2.svg"
+  },
+  {
+    "revision": "7cef5dfe42f6a560329095ef7f27fc05",
+    "url": "/cardflip/static/media/10H.7cef5dfe.svg"
+  },
+  {
+    "revision": "1cdfcf721392e387f0b0788ea330d7e0",
+    "url": "/cardflip/static/media/10S.1cdfcf72.svg"
+  },
+  {
+    "revision": "c5420578ede76bb8f120f11988047b99",
+    "url": "/cardflip/static/media/2C.c5420578.svg"
+  },
+  {
+    "revision": "d65caedd0e7bebdec42181710ed4a42e",
+    "url": "/cardflip/static/media/2D.d65caedd.svg"
+  },
+  {
+    "revision": "b4cd109d37f1bc3f09b556b2d4321046",
+    "url": "/cardflip/static/media/2H.b4cd109d.svg"
+  },
+  {
+    "revision": "fec604864b5365c69d64ecc8f87d17b9",
+    "url": "/cardflip/static/media/2S.fec60486.svg"
+  },
+  {
+    "revision": "73239df148134bb2e57ae6f7a63d3ce1",
+    "url": "/cardflip/static/media/3C.73239df1.svg"
+  },
+  {
+    "revision": "c2f29242e14227bee5e46790d6e38706",
+    "url": "/cardflip/static/media/3D.c2f29242.svg"
+  },
+  {
+    "revision": "329677ca2811d98b37b23c67443d9998",
+    "url": "/cardflip/static/media/3H.329677ca.svg"
+  },
+  {
+    "revision": "2044bbf9528815e1202f22547f820a1a",
+    "url": "/cardflip/static/media/3S.2044bbf9.svg"
+  },
+  {
+    "revision": "33eaeb868b26a309f62e1d74c46e03fd",
+    "url": "/cardflip/static/media/4C.33eaeb86.svg"
+  },
+  {
+    "revision": "9be266c586878e2d7eefda1bfcde0b3e",
+    "url": "/cardflip/static/media/4D.9be266c5.svg"
+  },
+  {
+    "revision": "66df38c52cc7fe18c075802447fb72cd",
+    "url": "/cardflip/static/media/4H.66df38c5.svg"
+  },
+  {
+    "revision": "14119e16f9b3c2166f594398c9e107df",
+    "url": "/cardflip/static/media/4S.14119e16.svg"
+  },
+  {
+    "revision": "66ab25a38677adee92128f08560e69e5",
+    "url": "/cardflip/static/media/5C.66ab25a3.svg"
+  },
+  {
+    "revision": "2bc5f35454c09892447a648088bacef9",
+    "url": "/cardflip/static/media/5D.2bc5f354.svg"
+  },
+  {
+    "revision": "d1e25bf5d9b40bab280c4b5f7b17de04",
+    "url": "/cardflip/static/media/5H.d1e25bf5.svg"
+  },
+  {
+    "revision": "44359031ac219f89ec708d7dfb0d087a",
+    "url": "/cardflip/static/media/5S.44359031.svg"
+  },
+  {
+    "revision": "135b52d19e63012c8762f3da47131990",
+    "url": "/cardflip/static/media/6C.135b52d1.svg"
+  },
+  {
+    "revision": "7fdf316ce4ad82d0b29c48112a8ba494",
+    "url": "/cardflip/static/media/6D.7fdf316c.svg"
+  },
+  {
+    "revision": "dcab0324657c1fd01d78cb2c33e6859a",
+    "url": "/cardflip/static/media/6H.dcab0324.svg"
+  },
+  {
+    "revision": "ea8f412cea7273d1750c211bfec01670",
+    "url": "/cardflip/static/media/6S.ea8f412c.svg"
+  },
+  {
+    "revision": "9d2066fffce0b66d83cf31885ad66d4d",
+    "url": "/cardflip/static/media/7C.9d2066ff.svg"
+  },
+  {
+    "revision": "7dbada36027a2025cc013313682adbdb",
+    "url": "/cardflip/static/media/7D.7dbada36.svg"
+  },
+  {
+    "revision": "0a3f1f5cdbec4fafb5bc8a39325c42e7",
+    "url": "/cardflip/static/media/7H.0a3f1f5c.svg"
+  },
+  {
+    "revision": "741b6ff3fa37cd0b22ab9bd0ad85dd34",
+    "url": "/cardflip/static/media/7S.741b6ff3.svg"
+  },
+  {
+    "revision": "53277935f639d1f6a3f848aef6950903",
+    "url": "/cardflip/static/media/8C.53277935.svg"
+  },
+  {
+    "revision": "19d1888a2247af44e394bfc321df53ef",
+    "url": "/cardflip/static/media/8D.19d1888a.svg"
+  },
+  {
+    "revision": "009927f264be76f796b5c086cda91bc8",
+    "url": "/cardflip/static/media/8H.009927f2.svg"
+  },
+  {
+    "revision": "5809acdf28721ec20a3c0d482959c55e",
+    "url": "/cardflip/static/media/8S.5809acdf.svg"
+  },
+  {
+    "revision": "bf7a9a2cfc32bd89dcb7a7d573a400ff",
+    "url": "/cardflip/static/media/9C.bf7a9a2c.svg"
+  },
+  {
+    "revision": "f0e46d7044f67735489581520edc50e1",
+    "url": "/cardflip/static/media/9D.f0e46d70.svg"
+  },
+  {
+    "revision": "6990f2296b8feb8dbf2ace39c48296ec",
+    "url": "/cardflip/static/media/9H.6990f229.svg"
+  },
+  {
+    "revision": "da3ff0ff1cb2f5d7375f1af791981a82",
+    "url": "/cardflip/static/media/9S.da3ff0ff.svg"
+  },
+  {
+    "revision": "ab9bfa4c13ac49bca4b8dc69cb973793",
+    "url": "/cardflip/static/media/AC.ab9bfa4c.svg"
+  },
+  {
+    "revision": "f4a414e5c0ff2932ca130bd790ecd9f1",
+    "url": "/cardflip/static/media/AD.f4a414e5.svg"
+  },
+  {
+    "revision": "5b2ba39897e75f32773ff5bf79f53a6d",
+    "url": "/cardflip/static/media/AH.5b2ba398.svg"
+  },
+  {
+    "revision": "25906b58b291ee6623767140276cdd1c",
+    "url": "/cardflip/static/media/AS.25906b58.svg"
+  },
+  {
+    "revision": "0db581a1516afa00538b3c0cb8499935",
+    "url": "/cardflip/static/media/JC.0db581a1.svg"
+  },
+  {
+    "revision": "3d55dc95eef2ff72795ddb5262fb14ca",
+    "url": "/cardflip/static/media/JD.3d55dc95.svg"
+  },
+  {
+    "revision": "e387d26f3cba79cbb4da8f50139d1558",
+    "url": "/cardflip/static/media/JH.e387d26f.svg"
+  },
+  {
+    "revision": "d4d9a1409190630cc0287a3d1e6d3b29",
+    "url": "/cardflip/static/media/JS.d4d9a140.svg"
+  },
+  {
+    "revision": "64f50e8a6c2f1ceda65568f292a9918c",
+    "url": "/cardflip/static/media/KC.64f50e8a.svg"
+  },
+  {
+    "revision": "3e358b62fa13ee65ccf65e8552e1a88a",
+    "url": "/cardflip/static/media/KD.3e358b62.svg"
+  },
+  {
+    "revision": "847c7e652cc70f5870c85639b66102ae",
+    "url": "/cardflip/static/media/KH.847c7e65.svg"
+  },
+  {
+    "revision": "02f4c73f4142dea9560c954d50cdceac",
+    "url": "/cardflip/static/media/KS.02f4c73f.svg"
+  },
+  {
+    "revision": "3466fa021a92d31e12679f1e45cdf26a",
+    "url": "/cardflip/static/media/QC.3466fa02.svg"
+  },
+  {
+    "revision": "e109f074bd6e800073919772424c0dce",
+    "url": "/cardflip/static/media/QD.e109f074.svg"
+  },
+  {
+    "revision": "8213a90f469941da1a70516fc23dcf7a",
+    "url": "/cardflip/static/media/QH.8213a90f.svg"
+  },
+  {
+    "revision": "dd9eebd66eec138cd5cdb9a647d21013",
+    "url": "/cardflip/static/media/QS.dd9eebd6.svg"
+  }
+]);
